a7acbd78ed73801b9fafe0405fc460f8c663b5c7
Your Name <tajempey@gmail.com>
2023-11-08 15:50:19 -0700

    should assign the proper stats to newCharacter object depending on selected class

--- __tests__/character-creation.test.js
+++ __tests__/character-creation.test.js
@@ -1,11 +1,19 @@
-import Character from '../src/character-creation';
+import { Character } from '../src/character-creation';
 
 describe('Character', () => {
 
-  test('should store passed inputs into a new object', () => {
-    const newCharacter = new Character(2,4,5);
-    expect(newCharacter.stat1).toEqual(2);
-    expect(newCharacter.stat2).toEqual(4);
-    expect(newCharacter.stat3).toEqual(5);
+  // test('should store passed inputs into a new object', () => {
+  //   const newCharacter = new Character(2,4,5);
+  //   expect(newCharacter.stat1).toEqual(2);
+  //   expect(newCharacter.stat2).toEqual(4);
+  //   expect(newCharacter.stat3).toEqual(5);
+  // });
+
+  test('should assign the proper stats to newCharacter object depending on selected class', () => {
+    const newCharacter = new Character("Knight");
+    expect(newCharacter.physicalAtk).toEqual(4);
+    expect(newCharacter.physicalDef).toEqual(4);
+    expect(newCharacter.magicAtk).toEqual(2);
+    expect(newCharacter.magicDef).toEqual(2);
   });
 });
--- src/character-creation.js
+++ src/character-creation.js
@@ -1,8 +1,27 @@
-export default function Character(stat1, stat2, stat3) {
-  this.stat1 = stat1;
-  this.stat2 = stat2;
-  this.stat3 = stat3;
+export function Character(characterClass) {
+  if (characterClass === "Knight") {
+    this.physicalAtk = 4;
+    this.magicAtk = 2;
+    this.physicalDef = 4;
+    this.magicDef = 2;
+  } else if (characterClass === "Sorcerer") {
+    this.physicalAtk = 2;
+    this.magicAtk = 6;
+    this.physicalDef = 2;
+    this.magicDef = 6;
+  } else if (characterClass === "Cleric") {
+    this.physicalAtk = 3;
+    this.magicAtk = 3;
+    this.physicalDef = 2;
+    this.magicDef = 4;
+  } else if (characterClass === "Misbegotten Wretch") {
+    this.physicalAtk = 1;
+    this.magicAtk = 1;
+    this.physicalDef = 1;
+    this.magicDef = 1;
+  }
 }
 
-let character = new Character(1,2,3);
+
+let character = new Character(1, 2, 3);
 console.log(character);
