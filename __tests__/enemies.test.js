b084c61920e3e9db6ca7746e717094ad6c1a6be5
Your Name <tajempey@gmail.com>
2023-11-09 12:06:02 -0700

    "enemyStats()" should determine how much damage an attack does to enemy hp based on class, attack type and enemy type

--- __tests__/enemies.test.js
+++ __tests__/enemies.test.js
@@ -1,10 +1,18 @@
 /* eslint-disable no-undef */
-import { enemyHealth } from "../src/enemyStats/enemies";
+import { enemyHealth, enemyStats } from "../src/enemyStats/enemies";
 
 describe('enemyHealth', () => {
 
   test('should subtract a specific amount from the enemy healthbar', () => {
-    const healthTest = enemyHealth(4, 12);
+    const healthTest = enemyHealth(4, 12, 0);
     expect(healthTest).toEqual(8);
+  });
+});
+
+describe('enemyStats', () => {
+
+  test('should determine how much damage an attack does to enemy hp', () => {
+    const healthBarTest = enemyStats("Knight", "Physical", "Skeleton");
+    expect(healthBarTest).toEqual(8);
   });
 });
